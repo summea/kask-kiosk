7944673fbf507c1385d5a4de211f4b7cbfcd6cd3
Khanh Nguyen <knultimate@live.com>
2014-02-03 14:23:35 -0800

    Update Employer.cs

--- Models/Employer.cs
+++ Models/Employer.cs
@@ -7,7 +7,7 @@ namespace Empty.Models
 {
     public class Employer
     {
-        public int employer_ID { get; set; }
-        public string employer_Name { get; set; }
+        public int Employer_ID { get; set; }
+        public string Employer_Name { get; set; }
     }
 }
