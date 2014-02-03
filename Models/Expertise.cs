e0d5c58b879678592903c0d46e997f2daa855f59
Khanh Nguyen <knultimate@live.com>
2014-02-03 14:23:49 -0800

    Update Expertise.cs

--- Models/Expertise.cs
+++ Models/Expertise.cs
@@ -7,7 +7,7 @@ namespace Empty.Models
 {
     public class Expertise
     {
-        public int applicant_ID { get; set; }
-        public int skill_ID { get; set; }
+        public int Applicant_ID { get; set; }
+        public int Skill_ID { get; set; }
     }
 }
