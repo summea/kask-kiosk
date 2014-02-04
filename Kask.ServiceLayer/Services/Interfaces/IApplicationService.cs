6560fd5a41841caa37ee635d07a6e0dca4efe5a6
khanh245 <knultimate@live.com>
2014-02-03 19:41:35 -0800

    Created Service and basic HTTP methods
    On Application-Service branch

--- Kask.ServiceLayer/Services/Interfaces/IApplicationService.cs
+++ Kask.ServiceLayer/Services/Interfaces/IApplicationService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using Kask.DAL.Models;
 
 namespace Kask.ServiceLayer.Services.Interfaces
 {
@@ -11,7 +12,43 @@ namespace Kask.ServiceLayer.Services.Interfaces
     [ServiceContract]
     public interface IApplicationService
     {
+        /// <summary>
+        /// Get Application by ID
+        /// </summary>
+        /// <param name="id">Application ID</param>
+        /// <returns>An application by ID</returns>
         [OperationContract]
-        string GetData(int value);
+        Applications GetApplication(int id);
+
+        /// <summary>
+        /// Gets all Applications
+        /// </summary>
+        /// <returns>All applications</returns>
+        [OperationContract]
+        Applications GetApplications();
+
+        /// <summary>
+        /// Create new Application
+        /// </summary>
+        /// <returns>A boolean whether a new application is created</returns>
+        [OperationContract]
+        Applications CreateApplication(Applications app);
+
+        /// <summary>
+        /// Update an Application by ID
+        /// </summary>
+        /// <param name="ID">Application's ID</param>
+        /// <returns>A boolean whether an application is updated</returns>
+        [OperationContract]
+        bool UpdateApplication(int ID);
+
+        /* ================ HTTP Delete /Application/id/ ================ */
+        /// <summary>
+        /// Delete an application by Application's ID
+        /// </summary>
+        /// <param name="ID">Application's ID</param>
+        /// <returns>A boolean whether an application is deleted or not</returns>
+        [OperationContract]
+        bool DeleteApplication(int ID);
     }
 }
