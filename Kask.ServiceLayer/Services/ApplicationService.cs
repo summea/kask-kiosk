cf001bffc03b564233a652e23a865ac0614c20ae
khanh245 <knultimate@live.com>
2014-02-03 18:59:52 -0800

    Initial ApplicationService in Kask.ServiceLayer

---
+++ Kask.ServiceLayer/Services/ApplicationService.cs
@@ -0,0 +1,14 @@
+﻿using Kask.ServiceLayer.Services.Interfaces;
+using System;
+
+namespace Kask.ServiceLayer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class ApplicationService : IApplicationService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+    }
+}
