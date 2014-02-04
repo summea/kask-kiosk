c34a88a005169bfbe5af3796f5fbaf360aa2df44
khanh245 <knultimate@live.com>
2014-02-03 19:41:35 -0800

    Created Service and basic HTTP methods
    On Application-Service branch

--- Kask.ServiceLayer/Services/ApplicationService.cs
+++ Kask.ServiceLayer/Services/ApplicationService.cs
@@ -1,4 +1,5 @@
 ﻿using Kask.ServiceLayer.Services.Interfaces;
+using Kask.DAL.Models;
 using System;
 
 namespace Kask.ServiceLayer
@@ -6,9 +7,34 @@ namespace Kask.ServiceLayer
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class ApplicationService : IApplicationService
     {
-        public string GetData(int value)
+        /* ================ HTTP GET /Application/id ================ */
+        public Applications GetApplication(int id)
         {
-            return string.Format("You entered: {0}", value);
+            throw new NotImplementedException();
+        }
+
+        /* ================ HTTP Get /Application/ ================ */
+        public Applications GetApplications()
+        {
+            throw new NotImplementedException();
+        }
+
+        /* ================ HTTP Post /Application/ ================ */
+        public Applications CreateApplication(Applications app)
+        {
+            throw new NotImplementedException();
+        }
+
+        /* ================ HTTP Put /Application/id/ ================ */
+        public bool UpdateApplication(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        /* ================ HTTP Delete /Application/id/ ================ */
+        public bool DeleteApplication(int ID)
+        {
+            throw new NotImplementedException();
         }
     }
 }
