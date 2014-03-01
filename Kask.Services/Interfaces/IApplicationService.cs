b74d49a84abe27f3e12d69537dcc8e1b448eee52
khanh245 <knultimate@live.com>
2014-03-01 13:10:37 -0800

    Service Updated (Applicant, application, applied)
    These are from Views branch

--- Kask.Services/Interfaces/IApplicationService.cs
+++ Kask.Services/Interfaces/IApplicationService.cs
@@ -5,7 +5,6 @@ using System.ServiceModel;
 
 namespace Kask.Services.Interfaces
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IApplicationService
     {
