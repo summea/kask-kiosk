8fe1c46a3359112c58605f9d2119eb704858a6ac
khanh245 <knultimate@live.com>
2014-03-01 13:10:37 -0800

    Service Updated (Applicant, application, applied)
    These are from Views branch

--- Kask.Services/Interfaces/IApplicantService.cs
+++ Kask.Services/Interfaces/IApplicantService.cs
@@ -8,44 +8,22 @@ namespace Kask.Services.Interfaces
     [ServiceContract]
     public interface IApplicantService
     {
-        /// <summary>
-        /// Get Applicant by ID
-        /// </summary>
-        /// <param name="id">Applicant ID</param>
-        /// <returns>Applicant</returns>
         [OperationContract]
         [FaultContract(typeof(KaskServiceException))]
         Applicant GetApplicantByID(int id);
 
-        /// <summary>
-        /// Get all Applicants
-        /// </summary>
-        /// <returns>List of Applicants</returns>
         [OperationContract]
         [FaultContract(typeof(KaskServiceException))]
         IList<Applicant> GetApplicants();
 
-        /// <summary>
-        /// Create New Applicant
-        /// </summary>
-        /// <returns>A boolean whether the new applicant is created</returns>
         [OperationContract]
         [FaultContract(typeof(KaskServiceException))]
         bool CreateApplicant(Applicant a);
 
-        /// <summary>
-        /// Update an applicant
-        /// </summary>
-        /// <returns>A boolean whether the applicant is updated</returns>
         [OperationContract]
         [FaultContract(typeof(KaskServiceException))]
         bool UpdateApplicant(Applicant newApp);
 
-        /// <summary>
-        /// Delete an applicant by ID
-        /// </summary>
-        /// <param name="ID">Applicant ID</param>
-        /// <returns>A boolean whether an applicant is deleted</returns>
         [OperationContract]
         [FaultContract(typeof(KaskServiceException))]
         bool DeleteApplicant(int ID);
