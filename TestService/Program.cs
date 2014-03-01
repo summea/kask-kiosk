c04b197f04e87783c0830cb95c1688e126c97461
datanets <Andrew.Summers@oit.edu>
2014-03-01 14:31:08 -0800

    little updates

--- TestService/Program.cs
+++ TestService/Program.cs
@@ -13,9 +13,11 @@ namespace TestService
     class Program
     {
         static void Main(string[] args)
-        {
+        {          
             ApplicationServiceClient client = new ApplicationServiceClient();
             IList<Application> apps = client.GetApplications();
+            ApplicantServiceClient client2 = new ApplicantServiceClient();
+            IList<Applicant> applicants = client2.GetApplicants();
             Console.ReadLine();
         }
     }
