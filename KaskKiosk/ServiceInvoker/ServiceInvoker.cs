8c938bc4c30e26ccc8c63e19a3c0e7a9cffa6753
khanh245 <knultimate@live.com>
2014-03-03 12:32:41 -0800

    Added Service Invoker Class
    This class is used to create and consume services

---
+++ KaskKiosk/ServiceInvoker/ServiceInvoker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+namespace KaskKiosk.Invoker
+{
+    public enum KaskServiceType
+    {
+        APPLICATION = 0,
+        APPLICANT = 1,
+        APPLIED = 2,
+        UNKNOWN = 0xff
+    };
+
+    public static class ServiceInvoker
+    {
+        private static readonly Dictionary<Type, ClientBase<Type>> m_Services = new Dictionary<Type, ClientBase<Type>>();
+
+        public static T Resolve<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
