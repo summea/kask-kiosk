077666939e2b81cee1b704a66fccb0a9678dd7ee
datanets <Andrew.Summers@oit.edu>
2014-03-05 11:42:41 -0800

    view updates

--- KaskKiosk/App_Start/RouteConfig.cs
+++ KaskKiosk/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace KaskKiosk
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "App", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Welcome", id = UrlParameter.Optional }
             );
         }
     }
