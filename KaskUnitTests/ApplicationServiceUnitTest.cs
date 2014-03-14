16b7db946c710dcdbee7edad67ff7d571c60f486
Smahanita <sdouyeb@hotmail.com>
2014-03-14 00:57:34 -0700

    Added part of the ApplicationService Unit tests

---
+++ KaskUnitTests/ApplicationServiceUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kask.Services.DAO;
+using Kask.Services.Interfaces;
+
+namespace Kask.UnitTests
+{
+    [TestClass]
+    public class ApplicationServiceUnitTest
+    {
+        public IApplicationService applicationService;
+        public ApplicationDAO applicationA;
+        public ApplicationDAO applicationB;
+        public ApplicationDAO applicationC;
+        public ApplicationDAO applicationD;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            applicationService = new MockApplicationService();
+            applicationA = new ApplicationDAO();
+            applicationB = new ApplicationDAO();
+            applicationC = new ApplicationDAO();
+            applicationD = new ApplicationDAO();
+        }
+
+        [TestMethod]
+        public void Test_GetApplicationByID()
+        {
+            applicationService.CreateApplication(applicationA);
+            applicationService.CreateApplication(applicationB);
+           // Assert.IsNull(applicationService.GetApplicationByID(1));
+        }
+    }
+}
---
+++ KaskUnitTests/MockServices/MockApplicationService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Kask.Services.DAO;
+using Kask.Services.Interfaces;
+
+namespace Kask.UnitTests
+{
+    class MockApplicationService: IApplicationService
+    {
+        List<ApplicationDAO> Applications = new List<ApplicationDAO>();
+        List<ApplicantDAO> Applicants = new List<ApplicantDAO>();
+       
+        public ApplicationDAO GetApplicationByID(int id)
+        {
+            foreach (var application in Applications)
+                if (application.ApplicationID == id)
+                    return application;
+            throw new Exception("Applicant not found");
+        }
+
+        public IList<ApplicationDAO> GetApplicationsByName(string first, string last, string ssn)
+        {
+            foreach (var applicant in Applicants)
+                if (applicant.FirstName == first || applicant.LastName == last || applicant.SSN == ssn)
+                {
+                    Applications.Add(GetApplicationByID(applicant.ID));
+                    return Applications;
+                }
+            throw new Exception("Applicant not found");
+        }
+
+        public IList<ApplicationDAO> GetApplications()
+        {
+            return Applications;
+        }
+
+        public bool CreateApplication(ApplicationDAO app)
+        {
+            Applications.Add(app);
+            return true;
+        }
+
+        public bool UpdateApplication(ApplicationDAO newApp)
+        {
+            foreach (var application in Applications)
+                if (application.ApplicationID == newApp.ApplicationID)
+                {
+                    Applications.Remove(application);
+                    Applications.Add(newApp);
+                    return true;
+                }
+            return false;
+        }
+
+        public bool DeleteApplication(int ID)
+        {
+            foreach (var application in Applications)
+                if (application.ApplicationID == ID)
+                {
+                    Applications.Remove(application);
+                    return true;
+                }
+            return false;
+        }
+    }
+}
