20235b3c3599759ac4b40aa26271c8902f789987
khanh245 <knultimate@live.com>
2014-04-13 19:10:12 -0700

    Temp commit for service refactoring

--- Kask.Services/Exceptions/KaskService.Exception.cs
+++ Kask.Services/Exceptions/KaskService.Exception.cs
@@ -6,6 +6,17 @@ using System.Web;
 
 namespace Kask.Services.Exceptions
 {
+    [DataContract(Name="KaskServiceType")]
+    public enum ServiceExceptionType
+    {
+        [EnumMember]
+        UNAUTHORIZED = 0x01,
+        [EnumMember]
+        INVALIDID = 0x02,
+        [EnumMember]
+        UNKNOWN = 0xff
+    };
+
     [DataContract]
     public class KaskServiceException
     {
