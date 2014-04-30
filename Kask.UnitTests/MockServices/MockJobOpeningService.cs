8059391550320d8a2dcf20f9bb7fc87d633012bd
khanhnguyen2 <khanh.nguyen2@oit.edu>
2014-04-29 18:50:45 -0700

    k2 updates to master

---
+++ Kask.UnitTests/MockServices/MockJobOpeningService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kask.Services.DAO;
+using Kask.Services.Interfaces;
+
+namespace KaskUnitTests.MockServices
+{
+    public class MockJobOpeningService : IJobOpeningService
+    {
+        List<JobOpeningDAO> JobOpenings = new List<JobOpeningDAO>();
+
+        internal void SetUp()
+        {
+            JobOpeningDAO jobopening1 = new JobOpeningDAO() { JobOpeningID = 1, JobID = 1, Approved = 1 };
+            JobOpeningDAO jobopening2 = new JobOpeningDAO() { JobOpeningID = 2, JobID = 2, Approved = 1 };
+            JobOpeningDAO jobopening3 = new JobOpeningDAO() { JobOpeningID = 3, JobID = 3, Approved = 1 };
+
+            JobOpenings.Add(jobopening1);
+            JobOpenings.Add(jobopening2);
+            JobOpenings.Add(jobopening3);
+        }
+
+        public JobOpeningDAO GetJobOpeningByID(int id)
+        {
+            foreach (var jo in JobOpenings)
+                if (jo.JobOpeningID == id)
+                    return jo;
+            throw new Exception("JobOpening not found..");
+        }
+
+        public IList<JobOpeningDAO> GetJobOpeningsByStoreID(int storeID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<JobOpeningDAO> GetJobOpenings()
+        {
+            return JobOpenings;
+        }
+
+        public bool CreateJobOpening(JobOpeningDAO e)
+        {
+            JobOpenings.Add(e);
+            return true;
+        }
+
+        public bool UpdateJobOpening(JobOpeningDAO newEmp)
+        {
+            foreach(var jo in JobOpenings)
+                if(jo.JobOpeningID == newEmp.JobOpeningID)
+                {
+                    JobOpenings.Remove(jo);
+                    JobOpenings.Add(newEmp);
+                    return true;
+                }
+            return false;
+        }
+
+        public bool DeleteJobOpening(int id)
+        {
+            foreach(var jo in JobOpenings)
+                if(jo.JobOpeningID == id)
+                {
+                    JobOpenings.Remove(jo);
+                    return true;
+                }
+            return false;
+        }
+
+        private void initializeJobOpening(JobOpeningDAO jobOpening, int id, int jobOpeningID, int jobID, int approved)
+        {
+            jobOpening.ID = id;
+            jobOpening.JobOpeningID = jobOpeningID;
+            jobOpening.JobID = jobID;
+            jobOpening.Approved = approved;
+        }
+    }
+}
---
+++ Kask.UnitTests/ServiceUnitTests/JobOpeningServiceUnitTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kask.Services.DAO;
+using Kask.Services.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KaskUnitTests.MockServices;
+
+namespace Kask.UnitTests
+{
+    [TestClass]
+    public class JobOpeningServiceUnitTest
+    {
+        public MockJobOpeningService jobOpeningService = new MockJobOpeningService();
+        List<JobOpeningDAO> JobOpenings = new List<JobOpeningDAO>();
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            jobOpeningService.SetUp();
+        }
+
+        [TestMethod]
+        [Description("Happy test")]
+        public void HappyTest_GetJobOpeningByID()
+        {
+            Assert.IsNotNull(jobOpeningService.GetJobOpeningByID(1));
+        }
+
+        [TestMethod]
+        [Description("Fail GetJobOpeningByID")]
+        [ExpectedException(typeof(Exception))]
+        public void Test_FailGetJobOpeningByID()
+        {
+            Assert.IsNull(jobOpeningService.GetJobOpeningByID(5));
+        }
+
+        [TestMethod]
+        public void Test_GetJobOpenings()
+        {
+            List<JobOpeningDAO> list = jobOpeningService.GetJobOpenings() as List<JobOpeningDAO>;
+            Assert.IsNotNull(list);
+        }
+
+        [TestMethod]
+        public void Test_CreateJobOpening()
+        {
+            JobOpeningDAO jobopening4 = new JobOpeningDAO() { JobOpeningID = 4, JobID = 4, Approved = 1 };
+            jobOpeningService.CreateJobOpening(jobopening4);
+            Assert.IsNotNull(jobOpeningService.GetJobOpeningByID(4));
+            Assert.AreEqual(jobOpeningService.GetJobOpenings().Count, 4);
+        }
+
+        [TestMethod]
+        public void Test_UpdateJobOpening()
+        {
+            JobOpeningDAO jobopening4 = new JobOpeningDAO() { JobOpeningID = 3, JobID = 4, Approved = 0 };
+            jobOpeningService.UpdateJobOpening(jobopening4);
+            Assert.AreEqual(jobOpeningService.GetJobOpeningByID(3).Approved, 0);
+            Assert.AreEqual(jobOpeningService.GetJobOpenings().Count, 3);
+        }
+
+        [TestMethod]
+        public void Test_DeleteJobOpening()
+        {
+            jobOpeningService.DeleteJobOpening(3);
+            Assert.AreEqual(jobOpeningService.GetJobOpenings().Count, 2);
+        }
+    }
+}
