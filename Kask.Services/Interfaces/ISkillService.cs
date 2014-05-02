924b17bdfcc8b1d6dbc02eb323744213c62b7c8a
Khanh Nguyen <knultimate@live.com>
2014-05-02 10:39:24 -0700

    k updates to master

---
+++ Kask.Services/Interfaces/ISkillService.cs
@@ -0,0 +1,36 @@
+﻿using Kask.Services.DAO;
+using Kask.Services.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kask.Services.Interfaces
+{
+    [ServiceContract]
+    public interface ISkillService
+    {
+        [OperationContract]
+        [FaultContract(typeof(KaskServiceException))]
+        SkillDAO GetSkillByID(int id);
+
+        [OperationContract]
+        [FaultContract(typeof(KaskServiceException))]
+        IList<SkillDAO> GetSkills();
+
+        [OperationContract]
+        [FaultContract(typeof(KaskServiceException))]
+        bool CreateSkill(SkillDAO e);
+
+        [OperationContract]
+        [FaultContract(typeof(KaskServiceException))]
+        bool UpdateSkill(SkillDAO newEmp);
+
+        [OperationContract]
+        [FaultContract(typeof(KaskServiceException))]
+        bool DeleteSkill(int id);
+    }
+}
