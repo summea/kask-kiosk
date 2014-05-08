8af7b5f34f91731a40695f8b0614903a2f4d093f
khanhnguyen2 <khanh.nguyen2@oit.edu>
2014-05-07 21:01:07 -0700

    Skill and Expertise Tests

---
+++ Kask.UnitTests/MockServices/MockExpertiseService.cs
@@ -0,0 +1,78 @@
+﻿using Kask.Services.DAO;
+using Kask.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace KaskUnitTests.MockServices
+{
+    public class MockExpertiseService : IExpertiseService
+    {
+        List<ExpertiseDAO> Expertises = new List<ExpertiseDAO>();
+
+        internal void SetUp()
+        {
+            ExpertiseDAO expertise1 = new ExpertiseDAO() { ID = 1, ExpertiseID = 1, ApplicantID = 1, SkillID = 1 };
+            ExpertiseDAO expertise2 = new ExpertiseDAO() { ID = 2, ExpertiseID = 2, ApplicantID = 2, SkillID = 2 };
+            ExpertiseDAO expertise3 = new ExpertiseDAO() { ID = 3, ExpertiseID = 3, ApplicantID = 3, SkillID = 3 };
+
+            Expertises.Add(expertise1);
+            Expertises.Add(expertise2);
+            Expertises.Add(expertise3);
+        }
+
+        public ExpertiseDAO GetExpertiseByID(int id)
+        {
+            foreach (var expertise in Expertises)
+                if (expertise.ExpertiseID == id)
+                    return expertise;
+            throw new Exception("No Expertise Found");
+        }
+
+        public IList<ExpertiseDAO> GetExpertises()
+        {
+            return Expertises;
+        }
+
+        public IList<ExpertiseDAO> GetExpertisesByName(string first, string last, string ssn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateExpertise(ExpertiseDAO e)
+        {
+            Expertises.Add(e);
+            return true;
+        }
+
+        public bool UpdateExpertise(ExpertiseDAO newExp)
+        {
+            foreach(var expertise in Expertises)
+                if(expertise.ExpertiseID == newExp.ExpertiseID)
+                {
+                    Expertises.Remove(expertise);
+                    Expertises.Add(newExp);
+                    return true;
+                }
+            return false;
+        }
+
+        public bool DeleteExpertise(int id)
+        {
+            foreach(var expertise in Expertises)
+                if(expertise.ExpertiseID == id)
+                {
+                    Expertises.Remove(expertise);
+                    return true;
+                }
+            return false;
+        }
+
+        private void Initialize( ExpertiseDAO expertise, int id, int expertiseID, int applicantID, int skillID )
+        {
+            expertise.ID = id;
+            expertise.ExpertiseID = expertiseID;
+            expertise.ApplicantID = applicantID;
+            expertise.SkillID = skillID;
+        }
+    }
+}
---
+++ Kask.UnitTests/MockServices/MockSkillService.cs
@@ -0,0 +1,73 @@
+﻿using Kask.Services.DAO;
+using Kask.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace KaskUnitTests.MockServices
+{
+    public class MockSkillService : ISkillService
+    {
+        List<SkillDAO> Skills = new List<SkillDAO>();
+
+        internal void SetUp()
+        {
+            SkillDAO skill1 = new SkillDAO() { ID = 1, SkillID = 1, SkillName = "skillname1" };
+            SkillDAO skill2 = new SkillDAO() { ID = 2, SkillID = 2, SkillName = "skillname2" };
+            SkillDAO skill3 = new SkillDAO() { ID = 3, SkillID = 3, SkillName = "skillname3" };
+
+            Skills.Add(skill1);
+            Skills.Add(skill2);
+            Skills.Add(skill3);
+        }
+        public SkillDAO GetSkillByID(int id)
+        {
+            foreach (var skill in Skills)
+                if (skill.SkillID == id)
+                    return skill;
+            throw new Exception("Skill not found");
+        }
+
+        public IList<SkillDAO> GetSkills()
+        {
+            return Skills;
+        }
+
+        public bool CreateSkill(SkillDAO e)
+        {
+            Skills.Add(e);
+            return true;
+        }
+
+        public bool UpdateSkill(SkillDAO newEmp)
+        {
+            foreach(var skill in Skills)
+                if(skill.SkillID == newEmp.SkillID)
+                {
+                    Skills.Remove(skill);
+                    Skills.Add(newEmp);
+                    return true;
+                }
+            return false;
+        }
+
+        public bool DeleteSkill(int id)
+        {
+            foreach(var skill in Skills)
+            {
+                if(skill.SkillID == id)
+                {
+                    Skills.Remove(skill);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void Initialize( SkillDAO skill, int id, int skillID, string skillName )
+        {
+            skill.ID = id;
+            skill.SkillID = skillID;
+            skill.SkillName = skillName;
+        }
+    }
+}
