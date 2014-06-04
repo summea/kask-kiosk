7ee3da1c601e0590e53722ce45c0d9df57bab5c5
datanets <Andrew.Summers@oit.edu>
2014-06-04 14:56:25 -0700

    view updates
    - updated CSS and layout for Dashboard
    - updated CSS for New User Account (Register) page

--- KaskKiosk/Controllers/HomeController.cs
+++ KaskKiosk/Controllers/HomeController.cs
@@ -26,31 +26,53 @@ namespace KaskKiosk.Controllers
             string allowableActions = "";
             if (this.User.IsInRole("HiringManager"))
             {
-                allowableActions += "<a href=\"/App/Index\">All Applications</a><br>" +
-                                    "<a href=\"/Jobs/Index\">View Job Types</a> | " +
-                                    "<a href=\"/Jobs/Create\">Create New Job Type</a> | " +
-                                    "<a href=\"/JobOpenings/Index\">View Current Job Openings</a> | " +
-                                    "<a href=\"/JobOpenings/Create\">Request Job Opening</a><br>" + 
-                                    "<a href=\"/MCQuestions/Index\">View All Multiple Choice Questions</a> | " +
-                                    "<a href=\"/MCQuestions/Create\">Create New Multiple Choice Questions</a><br>" +
-                                    "<a href=\"/MCOptions/Index\">View All Multiple Choice Options</a> | " +
-                                    "<a href=\"/MCOptions/Create\">Create Multiple Choice Options</a><br>" + 
-                                    "<a href=\"/SAQuestions/Index\">View All Short Answer Questions</a> | " +
-                                    "<a href=\"/SAQuestions/Create\">Create New Short Answer Questions</a><br>" +
-                                    "<a href=\"/SAResponses/Index\">View All Short Answer Responses</a> | " +
-                                    "<a href=\"/SAResponses/Create\">Create Short Answer Responses</a><br>" +
-                                    "<a href=\"/Skills/Index\">View All Skills</a> | " +
-                                    "<a href=\"/Skills/Create\">Create New Skill</a>";
+                allowableActions += "<h3>Applications</h3>\n" +
+                                    "<ul>\n" +
+                                    "<li><a href=\"/App/Index\">View All Applications</a></li>\n" +
+                                    "</ul>\n" +
+                                    "<h3>Jobs</h3>\n" +
+                                    "<ul>\n" +
+                                    "<li><a href=\"/Jobs/Index\">View Job Types</a></li>\n" +
+                                    "<li><a href=\"/Jobs/Create\">Create New Job Type</a></li>\n" +
+                                    "</ul>\n" +
+                                    "<h3>Job Openings</h3>" +
+                                    "<ul>\n" +
+                                    "<li><a href=\"/JobOpenings/Index\">View Current Job Openings</a></li>\n" +
+                                    "<li><a href=\"/JobOpenings/Create\">Request Job Opening</a></li>\n" +
+                                    "</ul>\n" +
+                                    "<h3>Multiple Choice Questions</h3>\n" +
+                                    "<ul>\n" +
+                                    "<li><a href=\"/MCQuestions/Index\">View All Multiple Choice Questions</a></li>\n" +
+                                    "<li><a href=\"/MCQuestions/Create\">Create New Multiple Choice Questions</a></li>\n" +
+                                    "<li><a href=\"/MCOptions/Index\">View All Multiple Choice Options</a></li>\n" +
+                                    "<li><a href=\"/MCOptions/Create\">Create Multiple Choice Options</a></li>\n" +
+                                    "</ul>\n" +
+                                    "<h3>Short Answer Questions</h3>" +
+                                    "<ul>\n" +
+                                    "<li><a href=\"/SAQuestions/Index\">View All Short Answer Questions</a></li>\n" +
+                                    "<li><a href=\"/SAQuestions/Create\">Create New Short Answer Questions</a></li>\n" +
+                                    "<li><a href=\"/SAResponses/Index\">View All Short Answer Responses</a></li>\n" +
+                                    "<li><a href=\"/SAResponses/Create\">Create Short Answer Responses</a></li>\n" +
+                                    "</ul>\n" +
+                                    "<h3>Skills</h3>" +
+                                    "<ul>\n" +
+                                    "<li><a href=\"/Skills/Index\">View All Skills</a></li>\n" +
+                                    "<li><a href=\"/Skills/Create\">Create New Skill</li>\n" +
+                                    "</ul>\n";
             }
             else if (this.User.IsInRole("StoreManager"))
             {
-                allowableActions += "<a href=\"/Jobs/Index\">View Job Types</a> | " +
-                                    "<a href=\"/Jobs/Create\">Create New Job Type</a> | " +
-                                    "<a href=\"/JobOpenings/Index\">View Current Job Openings</a> | ";
+                allowableActions += "<h3>Job Openings</h3>" +
+                                    "<ul>\n" +
+                                    "<li><a href=\"/JobOpenings/Index\">View Current Job Openings</a></li>\n" +
+                                    "</ul>\n";
             }
             else
             {
-                allowableActions += "<a href=\"/App/Status\">View Application Status</a>";
+                allowableActions += "<h3>Applications</h3>" +
+                                    "<ul>\n" +
+                                    "<li><a href=\"/App/Status\">View Application Status</a></li>\n" +
+                                    "</ul>\n";
             }
             ViewBag.allowableActions = allowableActions;
             return View();
